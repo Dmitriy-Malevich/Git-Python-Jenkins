06e095a206dc80d80d43533a935c133a2754f3c8
Dmitriy <malevich_d@mzkt.by>
2022-10-13 10:41:58 +0300

    Test project deploy Jenkins

---
+++ Jenkins.py
@@ -0,0 +1,14 @@
+from ast import While
+from tkinter.tix import Tree
+
+
+name = []
+print("Press to exit 'q'")
+while True:
+    names = input("Enter your name: ")
+    if names == "q":
+        break
+    name.append(names)
+print("Complete list of names:")
+for f_name in name:
+    print(f_name)
